3e22eaac78ef6da056483323dfd71deddbe58555
Anu Anand <34204170+anu-anand@users.noreply.github.com>
2020-07-11 21:43:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-  
 console.log("Welcome to notes app. This is app.js");
 showNotes();
 
@@ -7,13 +6,22 @@ let addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", function(e) {
   let addTxt = document.getElementById("addTxt");
   let notes = localStorage.getItem("notes");
+  let importance = localStorage.getItem("importance")
+  
   if (notes == null) {
     notesObj = [];
+    impObj = [];
   } else {
     notesObj = JSON.parse(notes);
+    impObj = JSON.parse(importance)
+
   }
   notesObj.push(addTxt.value);
+  impObj.push("NO"); //initially not important
+  
   localStorage.setItem("notes", JSON.stringify(notesObj));
+  localStorage.setItem("importance", JSON.stringify(impObj));
+
   addTxt.value = "";
 //   console.log(notesObj);
   showNotes();
@@ -26,19 +34,37 @@ function showNotes() {
     notesObj = [];
   } else {
     notesObj = JSON.parse(notes);
+    impObj = JSON.parse(localStorage.getItem('importance'))
   }
   let html = "";
   notesObj.forEach(function(element, index) {
+    
+
     html += `
-            <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+            <div class="noteCard my-2 mx-2 card" style="width: 18rem; background-color: ${impObj[index]};">
                     <div class="card-body">
                         <h5 class="card-title">Note ${index + 1}</h5>
                         <p class="card-text"> ${element}</p>
                         <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
+                        <button id="${index}"onclick="addCss(this.id)" class="btn btn-info">Imp</button>
                     </div>
                 </div>`;
   });
+
   let notesElm = document.getElementById("notes");
+  
+//   let importance = localStorage.getItem("importance");
+//   if(importance == null){ impObj = []} else { impObj = JSON.parse(importance)}
+  
+//   impObj.forEach(function(element,index){
+//         if(!element) {
+//             console.log(element);
+//             console.log(notesElm[index])
+//             //notes(this.index).setAttribute('backgroundColor','yellow')
+//         }
+
+//   }) //loop and change attrin
+  
   if (notesObj.length != 0) {
     notesElm.innerHTML = html;
   } else {
@@ -46,19 +72,45 @@ function showNotes() {
   }
 }
 
+// function to pul importance
+function addCss(index){
+    // console.log(Number(index)+1)
+    // lets now pull the notesElement and set attribute
+    let notesElems = document.getElementById("notes").children;
+    notesElems= Array.from(notesElems)
+    //console.log(notesElems[index]);
+
+    selected = notesElems[index]
+    importance = localStorage.getItem('importance')
+    importance = JSON.parse(importance)
+    //console.log("before ",importance[index])
+    importance[index]= 'yellow'
+    //console.log('Changed : ',importance)
+    localStorage.setItem('importance',JSON.stringify(importance))
+    
+    showNotes()
+
+}
+
+
 // Function to delete a note
 function deleteNote(index) {
 //   console.log("I am deleting", index);
 
   let notes = localStorage.getItem("notes");
+  let importance = localStorage.getItem("importance");
   if (notes == null) {
     notesObj = [];
+    impObj = [];
   } else {
     notesObj = JSON.parse(notes);
+    impObj = JSON.parse(importance)
   }
 
   notesObj.splice(index, 1);
+  impObj.splice(index, 1);
   localStorage.setItem("notes", JSON.stringify(notesObj));
+  localStorage.setItem('importance',JSON.stringify(impObj))
   showNotes();
 }
 
@@ -73,7 +125,6 @@ search.addEventListener("input", function(){
         let cardTxt = element.getElementsByTagName("p")[0].innerText.toLowerCase();
         if(cardTxt.includes(inputVal)){
             element.style.display = "block";
-            // console.log(element);
         }
         else{
             element.style.display = "none";
@@ -85,7 +136,7 @@ search.addEventListener("input", function(){
 /*
 Further Features:
 1. Add Title
-2. Mark a note as Important
+2. Mark a note as Important  - done with Yellow color
 3. Separate notes by user
 4. Sync and host to web server 
 */ 
